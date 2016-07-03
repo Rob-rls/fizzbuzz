60d178432bf31f9c08071abcc067ed14849af231
Robert Summers <rsummers02@gmail.com>
2016-07-03 20:13:38 +0100

    fuzzbuzz complete

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,11 +6,14 @@ describe 'fizzbuzz' do
   end
 
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'fizz'
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
 
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
+  it 'returns 17 when passed 17' do
+    expect(fizzbuzz(17)).to eq 17
+  end
 end
